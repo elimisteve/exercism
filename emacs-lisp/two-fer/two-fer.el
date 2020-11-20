00ee2cd1e9a3c4603d3e1d2265ed74ba58390f55
Steve Phillips <steve@tryingtobeawesome.com>
2020-11-19 19:38:04 -0800

    Added working emacs-lisp/two-fer/two-fer.el

--- emacs-lisp/two-fer/two-fer.el
+++ emacs-lisp/two-fer/two-fer.el
@@ -3,6 +3,11 @@
 ;;; Commentary:
 
 ;;; Code:
+(defun two-fer (&optional name)
+  "My two-fer function"
+  (if (eq name nil)
+      "One for you, one for me."
+      (concat "One for " name ", one for me.")))
 
 (provide 'two-fer)
 ;;; two-fer.el ends here
