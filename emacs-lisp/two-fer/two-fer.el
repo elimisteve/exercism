520e7b73d0e5c25d2952cdd1e1606936845845a8
Steve Phillips <steve@tryingtobeawesome.com>
2020-11-19 19:49:42 -0800

    emacs-lisp/two-fer: Made it easier to read

--- emacs-lisp/two-fer/two-fer.el
+++ emacs-lisp/two-fer/two-fer.el
@@ -4,7 +4,10 @@
 
 ;;; Code:
 (defun two-fer (&optional name)
-  (concat "One for " (or name "you") ", one for me."))
+  (concat
+   "One for "
+   (or name "you")
+   ", one for me."))
 
 (provide 'two-fer)
 ;;; two-fer.el ends here
