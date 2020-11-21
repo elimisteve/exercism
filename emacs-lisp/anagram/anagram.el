df5749ae909ab0b6c621f2251a7e04ee486289f1
Steve Phillips <steve@tryingtobeawesome.com>
2020-11-20 22:22:37 -0800

    emacs-lisp/anagram: Made more efficient

--- emacs-lisp/anagram/anagram.el
+++ emacs-lisp/anagram/anagram.el
@@ -8,14 +8,16 @@
 (require 'seq)
 
 (defun anagrams-for (word list)
-  (seq-filter
-   (lambda (w)
-     (and
-      (not (equal w word))
-      (equal
-       (sort (split-string (downcase w) "") 'string-lessp)
-       (sort (split-string (downcase word) "") 'string-lessp))))
-   list))
+  (let ((word-chars-sorted
+       (sort (split-string (downcase word) "") 'string-lessp)))
+    (seq-filter
+     (lambda (w)
+       (and
+        (not (equal w word))
+        (equal
+         (sort (split-string (downcase w) "") 'string-lessp)
+         word-chars-sorted)))
+     list)))
 
 (provide 'anagrams-for)
 ;;; anagram.el ends here
