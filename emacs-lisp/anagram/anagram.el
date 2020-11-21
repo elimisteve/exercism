cc975b59a62188cb6f5373867f1dcff596a0850e
Steve Phillips <steve@tryingtobeawesome.com>
2020-11-20 22:19:07 -0800

    emacs-lisp/anagram: Solved!

--- emacs-lisp/anagram/anagram.el
+++ emacs-lisp/anagram/anagram.el
@@ -5,7 +5,17 @@
 ;;; Code:
 
 (require 'cl-lib)
+(require 'seq)
 
+(defun anagrams-for (word list)
+  (seq-filter
+   (lambda (w)
+     (and
+      (not (equal w word))
+      (equal
+       (sort (split-string (downcase w) "") 'string-lessp)
+       (sort (split-string (downcase word) "") 'string-lessp))))
+   list))
 
-(provide 'anagram)
+(provide 'anagrams-for)
 ;;; anagram.el ends here
